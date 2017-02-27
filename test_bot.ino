eff74b3feaa4a25b06a335f9b661d80289de3abf
AndreLagro <lagrotteria.andrea@gmail.com>
2017-02-27 22:01:34 +0100

    first commit

---
+++ test_bot.ino
@@ -0,0 +1,64 @@
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+#include <UniversalTelegramBot.h>
+
+
+// Initialize Wifi connection to the router
+char ssid[] = "xxxx";     // your network SSID (name)
+char password[] = "xxxx"; // your network key
+
+#define BOTtoken "xxxx"
+
+int Bot_mtbs = 1000; //mean time between scan messages
+long Bot_lasttime;   //last time messages' scan has been done
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+void setup() {
+  Serial.begin(9600);
+
+  // Set WiFi to station mode and disconnect from an AP if it was Previously
+  // connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  // Attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+
+}
+
+void loop() {
+  if (millis() > Bot_lasttime + Bot_mtbs)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while(numNewMessages) {
+      Serial.println("got response");
+      for (int i=0; i<numNewMessages; i++) {
+        bot.sendMessage(bot.messages[i].chat_id, "testo, "");
+      }
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+
+    Bot_lasttime = millis();
+  } 
+  // bot.sendMessage("id", "testo", ""); 
+
+}
